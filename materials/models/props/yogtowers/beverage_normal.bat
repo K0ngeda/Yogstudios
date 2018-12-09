b0897968dbffa00a7a66b574f6105555a751fecf
K0ngeda <matthewbuchowiec1@gmail.com>
2018-12-09 01:44:50 -0500

    Edited Models and Materials

---
+++ materials/models/props/yogtowers/beverage_normal.bat
@@ -0,0 +1,3 @@
+@echo on
+"E:\SteamLibrary\steamapps\common\GarrysMod\bin\vtex.exe" -game "E:\SteamLibrary\steamapps\common\GarrysMod\garrysmod" -mkdir -outdir "E:\SteamLibrary\steamapps\common\GarrysMod\garrysmod\materials\models\props\yogtowers" "E:/SteamLibrary/steamapps/common/GarrysMod/garrysmod/materials/models/props/yogtowers/beverage_normal.tga"
+@pause
