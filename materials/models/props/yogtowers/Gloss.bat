4e1c2d3d8df47971019d62ef008fe8481db1afc2
K0ngeda <matthewbuchowiec1@gmail.com>
2018-12-09 12:37:59 -0500

    Removed Excess Files
    .bat and .txt removed

--- materials/models/props/yogtowers/Gloss.bat
+++
@@ -1,3 +0,0 @@
-@echo on
-"E:\SteamLibrary\steamapps\common\GarrysMod\bin\vtex.exe" -game "E:\SteamLibrary\steamapps\common\GarrysMod\garrysmod" -mkdir -outdir "E:\SteamLibrary\steamapps\common\GarrysMod\garrysmod\materials\models\props\yogtowers" "E:/SteamLibrary/steamapps/common/GarrysMod/garrysmod/materials/models/props/yogtowers/Gloss.tga"
-@pause
