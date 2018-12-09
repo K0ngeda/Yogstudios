b7fe505d4fbb387e235b321a6a3fba8c69e24775
K0ngeda <matthewbuchowiec1@gmail.com>
2018-12-08 21:54:33 -0500

    Initial Upload
    Am I doing this right?

---
+++ models/props/yogtowers/kitchentable.bat
@@ -0,0 +1,4 @@
+@echo on
+REM Compiling using the Wall Worm Batch File...
+"E:\SteamLibrary\steamapps\common\GarrysMod\bin\studiomdl.exe" -game "E:\SteamLibrary\steamapps\common\GarrysMod\garrysmod" -nop4 "E:\SteamLibrary\steamapps\common\GarrysMod\garrysmod\models\props\yogtowers\kitchentable.qc"
+@pause
